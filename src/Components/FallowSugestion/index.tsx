5d37e01f0ab27e849f7400202d073318656e4845
Gabriel-biel <gabriel97gla98@gmail.com>
2021-10-15 22:21:07 -0300

    Finalizado o clone da pagina principal do twitter

---
+++ src/Components/FallowSugestion/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { 
+  Container, 
+  Avatar, 
+  Info, 
+  FallowButton 
+} from './styles';
+
+interface Props {
+  name:string,
+  nickname: string;
+}
+
+const FallowSugestion: React.FC<Props> = ({
+  name,
+  nickname
+}) => {
+  return (
+    <Container>
+      <div>
+        <Avatar />
+        <Info>
+          <strong>{name}</strong>
+          <span>{nickname}</span>
+        </Info>
+
+      </div>
+        <FallowButton outlined>Seguir</FallowButton>
+    </Container>
+  );
+}
+
+export default FallowSugestion;
--- src/Components/SideBar/index.tsx
+++ src/Components/SideBar/index.tsx
@@ -1,11 +1,74 @@
 import React from 'react';
+import StickyBox from 'react-sticky-box';
 
-import { Container } from './styles';
+import { 
+  Container,
+  SearchWrapper,
+  SearchInput,
+  SearchIcon,
+  Body
+} from './styles';
+
+import List from '../List';
+import News from '../News';
+import FallowSugestion from '../FallowSugestion';
 
 const SideBar: React.FC = () => {
   return (
     <Container>
-      
+      <SearchWrapper>
+        <SearchInput placeholder="Buscar no twitter" />
+        <SearchIcon />
+      </SearchWrapper>
+
+      <StickyBox>
+      <Body>
+        <List 
+          title="Talvez você curta"
+          elements={[
+            <FallowSugestion 
+            name="Vinicius Malafaia"
+            nickname="@vinnie"
+            />,
+            <FallowSugestion 
+            name="Samuel Andrade"
+            nickname="@samuka"
+            />,
+            <FallowSugestion 
+            name="Gabriel Lima"
+            nickname="@bielzin"
+            />
+          ]}
+        />
+        <List 
+          title="Talvez você curta"
+          elements={[
+            <News />,
+            <News />,
+            <News />,
+            <News />
+          ]}
+        />
+        <List 
+          title="Talvez você curta"
+          elements={[
+            <News />,
+            <News />,
+            <News />,
+            <News />
+          ]}
+        />
+        <List 
+          title="Talvez você curta"
+          elements={[
+            <News />,
+            <News />,
+            <News />,
+            <News />
+          ]}
+        />
+      </Body>
+      </StickyBox>
     </Container>
   );
 }
